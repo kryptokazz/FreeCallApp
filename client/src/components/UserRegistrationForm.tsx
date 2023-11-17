c0c0be2f310f6a8b6e26b41026d23445b8fea5b8
kryptokazz <c.leathamorrell@uqconnect.edu.au>
2023-11-17 23:07:33 +1000

    user registration front end

--- client/src/components/UserRegistrationForm.tsx
+++ client/src/components/UserRegistrationForm.tsx
@@ -1,12 +1,42 @@
+// UserRegistrationForm.jsx
 import React, { useState } from 'react';
 
-const UserRegistrationForm: React.FC = () => {
+const UserRegistrationForm = () => {
   const [username, setUsername] = useState('');
-  const [profileName, setProfileName] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Perform logic to submit the form data to the server
+
+    // Basic validation
+    if (!username || !password || password !== confirmPassword) {
+      setErrorMessage('Please fill in all fields and ensure passwords match.');
+      return;
+    }
+
+    try {
+      const response = await fetch('http://localhost:5000/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.ok) {
+        // Handle successful registration, e.g., redirect to a new page
+        console.log('Registration successful');
+      } else {
+        // Handle failed registration, display an error message
+        const errorData = await response.json();
+        setErrorMessage(errorData.error || 'Registration failed');
+      }
+    } catch (error) {
+      console.error('Error during registration:', error);
+      setErrorMessage('Internal server error');
+    }
   };
 
   return (
@@ -16,12 +46,22 @@ const UserRegistrationForm: React.FC = () => {
         <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
       </label>
       <label>
-        Profile Name:
-        <input type="text" value={profileName} onChange={(e) => setProfileName(e.target.value)} />
+        Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <label>
+        Confirm Password:
+        <input
+          type="password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
       </label>
       <button type="submit">Register</button>
+      {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
     </form>
   );
 };
 
 export default UserRegistrationForm;
+
