4834f776c63720aa393ae02f8fd7206502d364a1
kryptokazz <c.leathamorrell@uqconnect.edu.au>
2023-12-11 12:53:09 +1000

    Implement CORS in server and update client for React 18 compatibility

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,14 @@
 // main.tsx
 import React from 'react';
-import { createRoot } from 'react-dom';
+import { createRoot } from 'react-dom/client'; // Updated import
 import App from './App';
 
-createRoot(document.getElementById('root')).render(
+const container = document.getElementById('root'); // Corrected function name
+const root = createRoot(container); // Create a root.
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
 
