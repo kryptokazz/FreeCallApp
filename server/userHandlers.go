8d0a92cabe62b186c7e6768979862c53bc895e52
kryptokazz <c.leathamorrell@uqconnect.edu.au>
2023-12-17 20:30:06 +1000

    Move structs to models file and add some CORS for handlers

--- server/userHandlers.go
+++ server/userHandlers.go
@@ -5,6 +5,7 @@ package main
 import (
     "encoding/json"
     "net/http"
+    "golang.org/x/crypto/bcrypt"
     "github.com/gorilla/mux"
     "database/sql"
 
@@ -43,19 +44,31 @@ func getUsers(w http.ResponseWriter, r *http.Request) {
 }
 
 func createUser(w http.ResponseWriter, r *http.Request) {
-    var newUser struct {
-        Username     string `json:"username"`
-        ProfileName  string `json:"profile_name"`
-    }
+	
+    w.Header().Set("Access-Control-Allow-Origin", "*") // Allows requests from any origin
+    w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS, POST") // Adjust as needed
+    w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization") // Adjust as needed
+
+   var newUser UserRegisterType  
+    
     err := json.NewDecoder(r.Body).Decode(&newUser)
     if err != nil {
         http.Error(w, err.Error(), http.StatusBadRequest)
         return
     }
 
+     hashedPassword, err := bcrypt.GenerateFromPassword([]byte(newUser.Password), bcrypt.DefaultCost)
+    if err != nil {
+        http.Error(w, "Failed to hash password", http.StatusInternalServerError)
+        return
+    }
+
     db := connectDB()
     defer db.Close()
 
+
+
+     // Store the hashed password in the UserAuth table
     var userID int
     err = db.QueryRow("INSERT INTO Users (username, profile_name) VALUES ($1, $2) RETURNING user_id", newUser.Username, newUser.ProfileName).Scan(&userID)
     if err != nil {
@@ -63,6 +76,11 @@ func createUser(w http.ResponseWriter, r *http.Request) {
         return
     }
 
+     _, err = db.Exec("INSERT INTO UserAuth (user_id, username, password_hash) VALUES ($1, $2, $3)", userID, newUser.Username, string(hashedPassword))
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
     json.NewEncoder(w).Encode(map[string]int{"user_id": userID})
 }
 
