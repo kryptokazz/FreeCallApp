42fead9cc2dd46a24ed99361d83bd13591659e5c
kryptokazz <c.leathamorrell@uqconnect.edu.au>
2023-12-22 22:00:44 +1000

    Visual improvements for signup page

--- client/src/components/User/UserRegistrationForm.tsx
+++ client/src/components/User/UserRegistrationForm.tsx
@@ -1,109 +1,34 @@
-// UserRegistrationForm.jsx
-import React, { useState } from 'react';
+import React from 'react';
 import './UserRegistrationForm.css';
 
-
-
 const UserRegistrationForm = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [profileName, setProfileName] = useState(''); // Added state for profile name
-  const [email, setEmail] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    // Basic validation
-    if (!username || !password || password !== confirmPassword || !profileName || !email) {
-      setErrorMessage('Please fill in all fields and ensure passwords match.');
-      return;
-    }
-
-    try {
-      const response = await fetch('http://localhost:5000/users', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password, profile_name: profileName, email }),
-      });
-
-      if (response.ok) {
-        // Handle successful registration, e.g., redirect to a new page
-        console.log('Registration successful');
-      } else {
-        // Handle failed registration, display an error message
-        const errorData = await response.json();
-        setErrorMessage(errorData.error || 'Registration failed');
-      }
-    } catch (error) {
-      console.error('Error during registration:', error);
-      setErrorMessage('Internal server error');
-    }
-  };
-
   return (
-    <form className="form-container" onSubmit={handleSubmit}>
-      <div className="form-field">
-        <label>
-          Username:
-          <input
-            className="form-input"
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
+    <div className="container">
+      <div className="branding-section">
+        {/* Branding content */}
+        <h1>Welcome to Our Service</h1>
       </div>
-      <div className="form-field">
-        <label>
-          Password:
-          <input
-            className="form-input"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
+      <div className="form-section">
+        <div className="signup-form">
+          <h2>Sign Up</h2>
+          <form>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" name="email" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" name="password" required />
+            </div>
+            <div className="form-group">
+              <label htmlFor="confirm-password">Confirm Password</label>
+              <input type="password" id="confirm-password" name="confirm-password" required />
+            </div>
+            <button type="submit" className="submit-btn">Create Account</button>
+          </form>
+        </div>
       </div>
-      <div className="form-field">
-        <label>
-          Confirm Password:
-          <input
-            className="form-input"
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-        </label>
-      </div>
-      <div className="form-field">
-        <label>
-          Profile Name:
-          <input
-            className="form-input"
-            type="text"
-            value={profileName}
-            onChange={(e) => setProfileName(e.target.value)}
-          />
-        </label>
-     </div>
-       <div className="form-field">
-        <label>
-          Email:
-          <input
-            className="form-input"
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-     </div>
-      <button className="form-button" type="submit">Register</button>
-      {errorMessage && <div style={{ color: 'red' }}>{errorMessage}</div>}
-    </form>
+    </div>
   );
 };
 
